2886a2c4add5ed0ddd130fd052282c6b2a174195
Katsuyuki Nakamura <katsuyuki.nakamura@gmail.com>
2022-06-24 17:37:26 +0900

    http://xxx.xxx.xxx.xxx:5000で、温度、湿度、PM2.5を表示する

--- server.py
+++ server.py
@@ -1,11 +1,27 @@
+import os
+from c300s import C300S
+from h5051 import H5051
 from flask import Flask
+
+vesync_id = os.environ['VESYNC_ID']
+vesync_password = os.environ['VESYNC_PASSWORD']
+vesync_timezone = os.environ['VESYNC_TIMEZONE']
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello world!</p>"
+def environment():
+    c300s = C300S(vesync_id, vesync_password, vesync_timezone)
+    h5051 = H5051()
+    h5051.update()
+
+    body = ""
+    body += "<p> 温度 " + str(h5051.temperature) + "度" + "</p>"
+    body += "<p> 湿度 " + str(h5051.humidity) + "％" + "</p>"
+    body += "<p> PM2.5 " + str(c300s.get_air_quality()) + "ug/㎥" + "</>"
+
+    return body
 
 
 app.run(host='0.0.0.0', port=5000)
